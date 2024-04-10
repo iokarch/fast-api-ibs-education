b7da78f735df9ab7eb524a7cec38bcd910a2354a
iokarch <ivankarch2020@gmail.com>
2024-04-11 01:38:28 +0300

    solving the first task

--- app/routes/task_1.py
+++ app/routes/task_1.py
@@ -17,8 +17,30 @@ router = APIRouter(tags=["Стажировка"])
 """
 @router.post("/find_in_different_registers", description="Задание_1. Удаление дублей")
 async def find_in_different_registers(words: list[str]) -> list[str]:
-    """Описание."""
+    """
+    Принимает массив слов, зависимых от регистра
+    Фильтрует дубли по регистру, исключая все остальные подобные слова, независящих от регистра
+    Возвращает уникальных список слов в нижнем регистре, которые прошли фильтрацию
+    """
 
     result = []
+
+    # Список уникальных слов в нижнем регистре
+    result = list(dict.fromkeys([item.lower() for item in words]))
+
+    # Список исключенный слов
+    ban_words = []
+
+    for index_1, word_1 in enumerate(words):
+        if word_1.lower() in ban_words:
+            continue
+        for index_2, word_2 in enumerate(words):
+            if (index_1 == index_2):
+                continue
+            if (word_1 == word_2):
+                if (word_1.lower() in result):
+                    result.pop(result.index(word_1.lower()))
+                ban_words.append(word_1.lower())
+                break
 
     return result
